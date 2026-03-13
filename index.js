5fa202cfb17d3f680adf6102090b1728e2e10aef
zachrtermanini-cmyk <zachrtermanini@gmail.com>
2026-03-13 13:10:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26666,6 +26666,10 @@ if(out.grounded){
     out.body.velocity.z/=dt*out.friction+1
 }
             
+            if((user.keys[' '] || jumpingButton) && !out.grounded && !out.isGliding){
+    out.body.velocity.y += out.jumpPower * 0.15
+}
+            
             let touchedJumped
 
             if(TIME-jumpingOnce<0.1){
